7e9e6aee2f5cc19279b87fab28cc23ce9c8c8c84
nivshapira211@gmail.com <niv@Nivs-MacBook-Air.local>
2026-01-24 17:52:07 +0200

    Created basic Feed

--- sunSeeker/src/App.tsx
+++ sunSeeker/src/App.tsx
@@ -1,16 +1,17 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainLayout from './components/layout/MainLayout';
-
+import Home from './pages/Home';
 
 function App() {
 return (
       <BrowserRouter>
         <Routes>
           <Route element={<MainLayout />}>
+            <Route path="/" element={<Home />} />
           </Route>
         </Routes>
       </BrowserRouter>
   );
 }
 
-export default App
+export default App;
